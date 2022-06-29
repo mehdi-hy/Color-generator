593feef0c38ea4ec64b2ff9929840dd3776e7f6a
mehdi-hy <85339536+mehdi-hy@users.noreply.github.com>
2022-06-29 14:00:18 -0700

    Add files via upload

---
+++ build/precache-manifest.32e7250d26ba91030caeae58ea636a93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5a2882f6fb36bbf63b9b077279989d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da39ea3c061cef3062d5",
+    "url": "/static/css/main.fb0efb37.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da39ea3c061cef3062d5",
+    "url": "/static/js/main.f6389edb.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
